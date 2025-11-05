72457b73ffb7398035283ed4d9bff8a9637cf28a
Daniil Vinichenko <d.vinichenko@ll-games.com>
2025-11-05 12:18:32 +0300

    init commit

--- src/main/java/org/example/GenerateExample.java
+++ src/main/java/org/example/GenerateExample.java
@@ -1,17 +1,157 @@
 package org.example;
 
-
-import org.example.classes.Example;
+import org.example.classes.*;
 import org.example.generator.Generator;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.List;
 
 public class GenerateExample {
     public static void main(String[] args) {
-        var gen = new Generator();
+        Generator gen = new Generator();
+        
+        System.out.println("======== Test of Generator ========\n");
+        testGeneration(gen, Example.class, "1. Class Example");
+        testGeneration(gen, Product.class, "2. Class Product");
+        testGeneration(gen, Shape.class, "3. Interface Shape"); // Должен найти либо Rectangle, либо Triangle
+        testGeneration(gen, Rectangle.class, "4. Class Rectangle");
+        testGeneration(gen, Triangle.class, "5. Class Triangle");
+        testGeneration(gen, BinaryTreeNode.class, "6. Recursive struture BinaryTreeNode");
+        testGeneration(gen, Cart.class, "7. Class with collection Cart");
+    }
+    
+    private static void testGeneration(Generator gen, Class<?> c, String testName) {
+        System.out.println("====================================");
+        System.out.println(testName);
+        System.out.println("====================================");
+        
         try {
-            Object generated = gen.generateValueOfType(Example.class);
-            System.out.println(generated);
+            Object generated = gen.generateValueOfType(c);
+            printObjectInfo(generated);
+            System.out.println("\n\tGOOD EXODUS!\n");
         } catch (Throwable e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            System.out.println("\n\tBAD EXODUS: \n" + e.getMessage() + "\n");
         }
+    }
+    
+    private static void printObjectInfo(Object obj) {
+        if (obj == null) {
+            System.out.println("!Null object!");
+            return;
+        }
+        
+        Class<?> c = obj.getClass();
+        
+        System.out.println("Class: " + c.getName());
+        System.out.println("Class name: " + c.getSimpleName());
+        
+        Class<?>[] interfaces = c.getInterfaces();
+        if (interfaces.length > 0) {
+            System.out.println("Interfaces:");
+            for (Class<?> anInterface : interfaces) {
+                System.out.println("- " + anInterface.getSimpleName());
+            }
+        }
+        
+        System.out.println("Fields:");
+        Field[] fields = c.getDeclaredFields();
+        if (fields.length == 0) {
+            System.out.println("- !No fields in class!");
+        } else {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                try {
+                    Object value = field.get(obj);
+                    String valueStr = formatValue(value);
+                    System.out.println("- " + Modifier.toString(field.getModifiers()) +
+                                     " " + field.getType().getSimpleName() +
+                                     " " + field.getName() + " = " + valueStr);
+                } catch (IllegalAccessException _) {}
+            }
+        }
+        
+        System.out.println("Methods calls and results:");
+        Method[] methods = c.getDeclaredMethods();
+        boolean hasMethods = false;
+        
+        for (Method method : methods) {
+            if (method.getParameterCount() == 0 &&
+                (method.getName().startsWith("get") || 
+                 method.getName().equals("getArea") || 
+                 method.getName().equals("getPerimeter") ||
+                 method.getName().equals("toString"))) {
+                
+                method.setAccessible(true);
+                try {
+                    Object result = method.invoke(obj);
+                    String resultStr = formatValue(result);
+                    System.out.println("- " + method.getName() + " -> " + resultStr);
+                    hasMethods = true;
+                } catch (Exception e) {
+                    System.out.println("- " + method.getName() + " -> error: " + e.getMessage());
+                    hasMethods = true;
+                }
+            }
+        }
+
+        if (!hasMethods) {
+            System.out.println("- !No available methods!");
+        }
+        
+        try {
+            Method toStringMethod = c.getMethod("toString");
+            String toStringResult = (String) toStringMethod.invoke(obj);
+            System.out.println("toString(): " + toStringResult);
+        } catch (Exception _) {}
+    }
+    
+    private static String formatValue(Object value) {
+        switch (value) {
+            case null -> {
+                return "null";
+            }
+            case String s -> {
+                return "\"" + value + "\"";
+            }
+            case List list -> {
+                return "List[" + list.size() + "] = " + list;
+            }
+            default -> {}
+        }
+
+        if (value.getClass().isArray()) {
+            return value.getClass().getSimpleName() + " array";
+        }
+        
+        if (!isPrimitiveOrWrapper(value.getClass())) {
+            return value.getClass().getSimpleName() + "@" + 
+                   Integer.toHexString(System.identityHashCode(value));
+        }
+        
+        return value.toString();
+    }
+    
+    private static boolean isPrimitiveOrWrapper(Class<?> c) {
+        return c.isPrimitive() || 
+               c == String.class ||
+               c == Integer.class ||
+               c == int.class ||
+               c == Double.class ||
+               c == double.class ||
+               c == Long.class ||
+               c == long.class ||
+               c == Float.class ||
+               c == float.class ||
+               c == Byte.class ||
+               c == byte.class ||
+               c == Short.class ||
+               c == short.class ||
+               c == Character.class ||
+               c == char.class ||
+               c == Boolean.class ||
+               c == boolean.class;
     }
 }
--- src/main/java/org/example/generator/Generator.java
+++ src/main/java/org/example/generator/Generator.java
@@ -1,18 +1,236 @@
 package org.example.generator;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Random;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Random;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 public class Generator {
+    public static final String CLASS = ".class";
+    public static final char POINT = '.';
+    public static final char STICK = '!';
+    public static final char SLASH = '/';
+    public static final String BLANK = "";
+    public static final String BINARY_TREE_NODE = "BinaryTreeNode";
+    private final Random random = new Random();
 
-    public Object generateValueOfType(Class<?> clazz) throws InvocationTargetException, InstantiationException, IllegalAccessException {
-        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+    /**
+     * "Маршрутизирует" или самостоятельно проводит генерацию заданного значения/объекта/интерфейса
+     */
+    public Object generateValueOfType(Class<?> c) throws InvocationTargetException, InstantiationException, IllegalAccessException, IOException, URISyntaxException, ClassNotFoundException {
+        if (c.isInterface()) {
+            return generateForInterface(c);
+        }
 
-        int randomConstructorIndex = new Random().nextInt(constructors.length);
-        Constructor<?> randomConstructor = constructors[randomConstructorIndex];
-        return randomConstructor.newInstance(111);
+        if (c == String.class) {
+            return generateString();
+        }
+        if (c == Character.class || c == char.class) {
+            return (char) (random.nextInt(26) + 'a');
+        }
+        if (c == Integer.class || c == int.class) {
+            return random.nextInt(1000);
+        }
+        if (c == Byte.class || c == byte.class) {
+            return (byte) random.nextInt(256);
+        }
+        if (c == Short.class || c == short.class) {
+            return (short) random.nextInt(Short.MAX_VALUE);
+        }
+        if (c == Long.class || c == long.class) {
+            return random.nextLong();
+        }
+        if (c == Float.class || c == float.class) {
+            return random.nextFloat() * 1000;
+        }
+        if (c == Double.class || c == double.class) {
+            return random.nextDouble() * 1000;
+        }
+        if (c == Boolean.class || c == boolean.class) {
+            return random.nextBoolean();
+        }
+
+        if (!c.isAnnotationPresent(Generatable.class)) {
+            throw new IllegalArgumentException("There is no @Generatable annotation in class " + c.getName());
+        }
+        return generateObject(c);
     }
 
+    /**
+     * Генерирует случайную строку от 1 до 64 символов
+     */
+    private String generateString() {
+        int l = random.nextInt(64) + 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < l; i++) {
+            char c = (char) (random.nextInt(26) + 'a');
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 
+    /**
+     * Генерирует реализацию интерфейса
+     */
+    private Object generateForInterface(Class<?> interfaceClass) throws InvocationTargetException, InstantiationException, IllegalAccessException, IOException, URISyntaxException, ClassNotFoundException {
+        String packageName = interfaceClass.getPackage().getName();
+        List<Class<?>> implementations = findImplementations(interfaceClass, packageName);
+
+        if (implementations.isEmpty()) {
+            throw new IllegalArgumentException("No @Generatable implementations found for interface " + interfaceClass.getName());
+        }
+
+        Class<?> selectedClass = implementations.get(random.nextInt(implementations.size()));
+        return generateObject(selectedClass);
+    }
+
+    /**
+     * Универсальный метод для поиска всех классов, реализующих интерфейс.
+     * Сканирует classpath на предмет классов в пакете
+     */
+    private List<Class<?>> findImplementations(Class<?> interfaceClass, String packageName) throws IOException, URISyntaxException, ClassNotFoundException {
+        List<Class<?>> implementations = new ArrayList<>();
+        String packagePath = packageName.replace(POINT, SLASH);
+
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        if (classLoader == null) {
+            classLoader = getClass().getClassLoader();
+        }
+
+        Enumeration<URL> resources = classLoader.getResources(packagePath);
+
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            String protocol = resource.getProtocol();
+
+            if ("file".equals(protocol)) {
+                findClassesInDirectory(resource, packageName, interfaceClass, implementations);
+            } else if ("jar".equals(protocol)) {
+                findClassesInJar(resource, packageName, interfaceClass, implementations);
+            }
+        }
+        
+        return implementations;
+    }
+
+    /**
+     * Сканирует директорию и находит все классы в пакете
+     */
+    private void findClassesInDirectory(URL resource, String packageName, Class<?> interfaceClass, List<Class<?>> implementations) throws URISyntaxException, IOException {
+        Path directory = Paths.get(resource.toURI());
+        if (Files.exists(directory) && Files.isDirectory(directory)) {
+            Files.walk(directory)
+                .filter(Files::isRegularFile)
+                .filter(path -> path.toString().endsWith(CLASS))
+                .forEach(path -> {
+                    String className = packageName + POINT +
+                        directory.relativize(path).toString().replace(SLASH, POINT).replace(CLASS, BLANK);
+                    try {
+                        checkAndAddClass(className, interfaceClass, implementations);
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+        }
+    }
+
+    /**
+     * Сканирует JAR'ник и находит все классы в пакете
+     */
+    private void findClassesInJar(URL resource, String packageName, Class<?> interfaceClass, List<Class<?>> implementations) throws ClassNotFoundException {
+        String packagePath = packageName.replace(POINT, SLASH);
+        String resourcePath = resource.getPath();
+        
+        int jarIndex = resourcePath.indexOf(STICK);
+        if (jarIndex == -1) {
+            return;
+        }
+        
+        String jarPath = resourcePath.substring(resourcePath.startsWith("file:") ? 5 : 0, jarIndex);
+        jarPath = java.net.URLDecoder.decode(jarPath, StandardCharsets.UTF_8);
+        
+        try (JarFile jar = new JarFile(jarPath)) {
+            Enumeration<JarEntry> entries = jar.entries();
+            
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                String entryName = entry.getName();
+                
+                if (entryName.startsWith(packagePath) && entryName.endsWith(CLASS) && !entryName.contains("$")) {
+                    String className = entryName.replace(SLASH, POINT).replace(CLASS, BLANK);
+                    checkAndAddClass(className, interfaceClass, implementations);
+                }
+            }
+        } catch (IOException _) {}
+    }
+
+    /**
+     * Проверяет класс и добавляет его в список, если он норм
+     */
+    private void checkAndAddClass(String className, Class<?> interfaceClass, List<Class<?>> implementations) throws ClassNotFoundException {
+        Class<?> c = Class.forName(className);
+        if (!c.isInterface() && interfaceClass.isAssignableFrom(c) && c.isAnnotationPresent(Generatable.class)) {
+            implementations.add(c);
+        }
+    }
+
+    private Object generateObject(Class<?> c) throws InvocationTargetException, InstantiationException, IllegalAccessException, IOException, URISyntaxException, ClassNotFoundException {
+        Constructor<?>[] constructors = c.getDeclaredConstructors();
+        
+        if (constructors.length == 0) {
+            throw new IllegalArgumentException("no constructors for class " + c.getName());
+        }
+
+        Constructor<?> selectedConstructor = constructors[random.nextInt(constructors.length)];
+        selectedConstructor.setAccessible(true);
+
+        Class<?>[] paramTypes = selectedConstructor.getParameterTypes();
+        Object[] params = new Object[paramTypes.length];
+        
+        for (int i = 0; i < paramTypes.length; i++) {
+            Class<?> paramType = paramTypes[i];
+            
+            if (paramType == List.class) {
+                Type genericType = selectedConstructor.getGenericParameterTypes()[i];
+                
+                // instanceof предложила idea, не я)
+                if (genericType instanceof ParameterizedType pt) {
+                    Type[] actualTypes = pt.getActualTypeArguments();
+                    
+                    if (actualTypes.length > 0 && actualTypes[0] instanceof Class<?> elementType) {
+                        List<Object> list = new ArrayList<>();
+                        
+                        int listSize = random.nextInt(3) + 1;
+                        
+                        for (int j = 0; j < listSize; j++) {
+                            list.add(generateValueOfType(elementType));
+                        }
+                        params[i] = list;
+                        continue;
+                    }
+                }
+            }
+            
+            if ((paramType == Integer.class || paramType.getName().contains(BINARY_TREE_NODE)) && random.nextDouble() < 0.3) {
+                params[i] = null;
+            } else {
+                params[i] = generateValueOfType(paramType);
+            }
+        }
+
+        return selectedConstructor.newInstance(params);
+    }
 }
